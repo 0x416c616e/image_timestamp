12f723173521921b292ac95e3c7a1e02f9335e7d
0x416c616e <29168822+0x416c616e@users.noreply.github.com>
2020-01-31 01:50:08 -0600

    image.py now puts date on picture, also supports cli args for font size and a second line of text under the date

--- image.py
+++ image.py
@@ -8,24 +8,40 @@ from PIL import ImageDraw
 import sys
 import argparse
 import asio
-from datetime import date, datetime
+import datetime
 
 def main():
     #filename, date, text, color, size
     parser = argparse.ArgumentParser(add_help=True)
     parser.add_argument('--filename', help='name of the image file to add the date to')
+    parser.add_argument('--text', help='add an additional message after the timestamp')
+    parser.add_argument('--size', help='set the size of the text to be put on the image')
     args = parser.parse_args()
     filename = str(args.filename)
     print("Filename: " + filename)
     if (asio.does_not_exist(filename)):
-        print("Error: file with filename " + filename + "does not exist")
+        print("Error: file with filename " + filename + " does not exist")
+        print("How to use this program: image.py --filename=photo.jpg")
+        print("Optionally: image.py --filename=photo.jpg --text='this line will go under the date'")
+        print("Optionally specify the size, i.e. image.py --size=32")
+        print("Use image.py --help for help")
         sys.exit(1)
     else:
-        today = "date goes here"
+        today = datetime.datetime.now()
+        formatted_date = today.strftime("%A, %B %d, %Y %I:%M%p")
+        full_message = formatted_date
+        if (args.text != ""):
+            full_message += "\n" + str(args.text)
+        print("Full message: " + str(full_message))
         photo = Image.open(filename)
         photodraw = ImageDraw.Draw(photo)
-        myfont = ImageFont.truetype("ariblk.ttf", 16)
-        photodraw.text((10,10), today, fill=(255,0,0), font=myfont)
+        fontsize = ""
+        if (args.size == ""):
+            fontsize = 64
+        else:
+            fontsize = int(args.size)
+        myfont = ImageFont.truetype("ariblk.ttf", fontsize)
+        photodraw.text((10,10), full_message, fill=(255,0,0), font=myfont)
         photo.save(filename)
         photo.close()
         
